f314d0644dd4e51b28793bd77af4e046136adc73
b_liu <b.liu@nki.nl>
2017-01-31 20:35:27 +0100

    remove users pk in users app v2

--- homeletter/users/urls.py
+++ homeletter/users/urls.py
@@ -8,11 +8,6 @@ urlpatterns = [
         name='update'
     ),
     url(
-        regex=r'^(?P<pk>\d+)/$',
-        view=views.UpdateView.as_view(),
-        name='update'
-    ),
-    url(
         regex=r'^password$',
         view=views.PasswordView.as_view(),
         name='password'
