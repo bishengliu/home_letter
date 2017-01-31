e593c4d965e0576525c8ae8cbecffdf54f4f3546
b_liu <b.liu@nki.nl>
2017-01-31 19:49:12 +0100

    add urls for letters app

--- homeletter/letters/urls.py
+++ homeletter/letters/urls.py
@@ -4,15 +4,45 @@ from . import views
 urlpatterns = [
     # manage letter categories
     url(
-        regex=r'^$',
+        regex=r'^category$',
         view=views.CategoryListView.as_view(),
-        name='category'
+        name='list_category'
     ),
-
     url(
         regex=r'^add_category$',
         view=views.CategoryCreateView.as_view(),
         name='add_category'
     ),
+    url(
+        regex=r'^update_category/(?P<pk>\d+)/$',
+        view=views.CategoryUpdateView.as_view(),
+        name='update_category'
+    ),
+    url(
+        regex=r'^delete_category/(?P<pk>\d+)/$',
+        view=views.CategoryDeleteView.as_view(),
+        name='delete_category'
+    ),
     # manage letters
+    url(
+        regex=r'^$',
+        view=views.LetterListView.as_view(),
+        name='list_letter'
+    ),
+    url(
+        regex=r'^add$',
+        view=views.LetterCreateView.as_view(),
+        name='add_letter'
+    ),
+    url(
+        regex=r'^update/(?P<pk>\d+)/$',
+        view=views.LetterUpdateView.as_view(),
+        name='update_letter'
+    ),
+    url(
+        regex=r'^delete/(?P<pk>\d+)/$',
+        view=views.LetterDeleteView.as_view(),
+        name='delete_letter'
+    )
+
 ]
