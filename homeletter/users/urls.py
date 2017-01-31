4cee425a37599f151d4689e2d1c2d6617e424781
b_liu <b.liu@nki.nl>
2017-01-31 14:46:43 +0100

    change the urls for user management v2

--- homeletter/users/urls.py
+++ homeletter/users/urls.py
@@ -1,24 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-'''
-# registration
-    url(
-        regex=r'^register$',
-        view=views.RegisterView.as_view(),
-        name='register'
-    ),
-    url(
-        regex=r'^logout$',
-        view=views.LogoutView.as_view(),
-        name='logout'
-    ),
-    url(
-        regex=r'^login$',
-        view=views.LoginView.as_view(),
-        name='login'
-    ),
-'''
 urlpatterns = [
     url(
         regex=r'^(?P<pk>\d+)/$',
