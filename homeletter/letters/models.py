e6ed330eadd6388f2f6ea57120e3a1ac0aa012db
b_liu <b.liu@nki.nl>
2017-02-11 00:17:50 +0100

    upload letter to dynamic folders

--- homeletter/letters/models.py
+++ homeletter/letters/models.py
@@ -6,17 +6,15 @@ from django.urls import reverse
 from datetime import datetime
 import os
 
-"""
-#/letters/2017/ABN_AMRO
-def upload_path_handler():
-    # return "letters/{year}/".format(year=str(datetime.now().year))
-    return os.path.join("letters/", str(datetime.now().year), "/")
-"""
+
+# /letters/2017/ABN_AMRO
+def upload_path_handler(instance, filename):
+    return os.path.join('letters', str(datetime.now().year), str(Category.objects.get(pk=instance.category_id)), filename)
 
 class Letter(models.Model):
     category = models.ForeignKey(Category)
     name = models.CharField(max_length=250, unique=True)
-    file = models.FileField(upload_to="letters/", max_length=250, null=True, blank=True)
+    file = models.FileField(upload_to=upload_path_handler, max_length=250, null=True, blank=True)
     date = models.DateField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     favorite = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
