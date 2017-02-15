e208d10f9aa607073be605d9ed151a599c72148d
b_liu <b.liu@nki.nl>
2017-02-15 18:53:49 +0100

    prepare to deploy the application

--- homeletter/config/settings/production.py
+++ homeletter/config/settings/production.py
@@ -4,7 +4,18 @@ Production Configurations
 """
 
 from .common import *
-
+# some variables, must be all capitals
+APPNAME = 'HomeLetter'
+APPVERSION = '0.0.1'
 
 # define app mode
 APP_MODE = 'production'
+
+# set this to allow use "if debug" in template
+INTERNAL_IPS = (
+    '0.0.0.0',
+    '127.0.0.1',
+)
+
+# message level
+MESSAGE_LEVEL = 10  # DEBUG
--- homeletter/config/settings/test.py
+++ homeletter/config/settings/test.py
@@ -5,5 +5,18 @@ Test Configurations
 
 from .common import *
 
+# some variables, must be all capitals
+APPNAME = 'HomeLetter'
+APPVERSION = '0.0.1'
+
 # define app mode
 APP_MODE = 'test'
+
+# set this to allow use "if debug" in template
+INTERNAL_IPS = (
+    '0.0.0.0',
+    '127.0.0.1',
+)
+
+# message level
+MESSAGE_LEVEL = 10  # DEBUG
