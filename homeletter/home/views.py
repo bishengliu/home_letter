54946243a8476c6afed4fb33239696fc237d170a
b_liu <b.liu@nki.nl>
2017-03-24 20:19:23 +0100

    update home

--- homeletter/home/views.py
+++ homeletter/home/views.py
@@ -10,5 +10,15 @@ class HomeIndexView(View):
     template_name = "home/home-index.html"
 
     def get(self, request, *args, **kwargs):
-        letters = Letter.objects.order_by('date')[:2]
+        letters = Letter.objects.order_by('-date')[:20]
+        for l in letters:
+            # get the favorite range
+            if l.favorite >= 0:
+                l.range1 = range(l.favorite)
+                l.range2 = range(5 - l.favorite)
+            # get the letter upload basename
+            if l.file is not None:
+                l.letter_name = l.file.name.split('/')[-1]
+            else:
+                l.letter_name = ""
         return render(request, self.template_name, {'letters': letters})
